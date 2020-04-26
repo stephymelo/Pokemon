660d396f4ea99fa8274e40c756010e47c247fa9a
stephym <60659457+stephymelo@users.noreply.github.com>
2020-04-26 17:44:18 -0500

    stuffFirst

---
+++ src/view/MainView.java
@@ -0,0 +1,28 @@
+package view;
+import processing.core.PApplet;
+
+public class MainView extends PApplet {
+	
+
+	public static void main(String[] args) {
+		PApplet.main(MainView.class.getName());
+	}
+
+	public void settings() {
+		size(1200, 800);
+	}
+
+	public void setup() {
+		background(0);
+	}
+
+	public void draw() {
+		
+
+	}
+
+}
+
+
+
+
