a8e8b4152be297003b9cd9723f247822851db9c9
Miguel Angel Rivera <angel1301999@gmail.com>
2020-05-26 16:06:28 -0500

    visibilizacion poquemones en mapa

--- src/controller/MapaController.java
+++ src/controller/MapaController.java
@@ -1,41 +1,31 @@
 package controller;
 
-
-
+import controlP5.Controller;
 import processing.core.PApplet;
 
 public class MapaController extends Controller {
 
-
-	
 	public MapaController(PApplet app) {
 		super(app);
-		
+
 	}
-	
+
 	public void pintarJugador() {
 		juego.jugadores();
-		 juego.pintarRival();
-	
-	
-		
-		
-		
+		juego.pintarRival();
+
 	}
+
 	public void caminar() {
 		juego.caminar();
 	}
-	
-	
-	
+
 	public void pintarMapa() {
-		 juego.pintarPokemonSalvaje();
-        juego.pintarMapa();
-        juego.encontrarPokemon();
-       
-    	
-       
+		juego.pintarPokemones();
+		juego.pintarPokemonSalvaje();
+		juego.pintarMapa();
+		juego.encontrarPokemon();
+
 	}
-	
-	
+
 }
